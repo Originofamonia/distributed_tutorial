c231dfd65a45f1a1df04f3622d5e2882c9717f45
Edward <anqiyuanms@hotmail.com>
2022-02-08 12:02:33 -0600

    from torch.nn.parallel import DistributedDataParallel as DDP

--- src/mnist-distributed.py
+++ src/mnist-distributed.py
@@ -7,7 +7,8 @@ import torchvision.transforms as transforms
 import torch
 import torch.nn as nn
 import torch.distributed as dist
-from apex.parallel import DistributedDataParallel as DDP
+from torch.nn.parallel import DistributedDataParallel as DDP
+# from apex.parallel import DistributedDataParallel as DDP
 from apex import amp
 
 
